cfe932a108df5db2a54d0792c777f012b1bff694
Nicholas Beach <nicholascbeach@gmail.com>
2019-01-08 06:41:54 -0500

    Added build scripts

--- src/main/java/com/nicholasbeach/starter/HelloController.java
+++ src/main/java/com/nicholasbeach/starter/HelloController.java
@@ -3,13 +3,8 @@ package com.nicholasbeach.starter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @RestController
 public class HelloController {
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/")
     public String hello() {
